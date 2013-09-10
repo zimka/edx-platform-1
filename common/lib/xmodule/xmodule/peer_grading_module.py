fdf86b9a8f101e591b0508508d6ff48d07a1f354
Joe Blaylock <jrbl@jrbl.org>
2013-09-10 14:31:37 -0700

    Use track_changes in peer_grading_problem
    * Refactor peer_grading_problem to lookup track_changes variable by
      problem location.
    
    Conflicts:
    	common/lib/xmodule/xmodule/peer_grading_module.py

--- common/lib/xmodule/xmodule/peer_grading_module.py
+++ common/lib/xmodule/xmodule/peer_grading_module.py
@@ -485,6 +485,15 @@ class PeerGradingModule(PeerGradingFields, XModule):
         })
         return html
 
+    def _find_corresponding_module_for_location(self, location):
+        '''find the peer grading module that links to the given location'''
+        try:
+            return modulestore().get_instance(self.system.course_id, location)
+        except Exception:
+            # the linked problem doesn't exist
+            log.error("Problem {0} does not exist in this course".format(location))
+            raise
+
     def peer_grading(self, _data=None):
         '''
         Show a peer grading interface
@@ -518,23 +527,11 @@ class PeerGradingModule(PeerGradingFields, XModule):
             log.exception("Could not contact peer grading service.")
             success = False
 
-
-        def _find_corresponding_module_for_location(location):
-            '''
-            find the peer grading module that links to the given location
-            '''
-            try:
-                return modulestore().get_instance(self.system.course_id, location)
-            except Exception:
-                # the linked problem doesn't exist
-                log.error("Problem {0} does not exist in this course".format(location))
-                raise
-
         good_problem_list = []
         for problem in problem_list:
             problem_location = problem['location']
             try:
-                descriptor = _find_corresponding_module_for_location(problem_location)
+                descriptor = self._find_corresponding_module_for_location(problem_location)
             except Exception:
                 continue
             if descriptor:
@@ -594,6 +591,11 @@ class PeerGradingModule(PeerGradingFields, XModule):
             return {'html': "", 'success': False}
         count_graded = response['count_graded']
         count_required = response['count_required']
+        descriptor = None
+        try:
+            descriptor = self._find_corresponding_module_for_location(problem_location)
+        except Exception:
+            pass
 
         ajax_url = self.ajax_url
         html = self.system.render_template('peer_grading/peer_grading_problem.html', {
@@ -605,7 +607,7 @@ class PeerGradingModule(PeerGradingFields, XModule):
             'count_required': count_required,
             # Checked above
             'staff_access': False,
-            'track_changes': True,                                                                                                 ### Pipe OE Boolean Here ####
+            'track_changes': getattr(descriptor, 'track_changes', False),
             'use_single_location': self.use_for_single_location,
         })
 
