a4940d8d55953bb1229c01b3062073449414dbf4
polesye <s2pak.anton@gmail.com>
2013-10-07 11:15:16 +0300

    Add RequireJs support.

--- cms/djangoapps/contentstore/features/component_settings_editor_helpers.py
+++ cms/djangoapps/contentstore/features/component_settings_editor_helpers.py
@@ -38,6 +38,8 @@ def create_component_instance(step, category, component_type=None, is_advanced=F
 @world.absorb
 def click_new_component_button(step, component_button_css):
     step.given('I have clicked the new unit button')
+
+    world.wait_for_requirejs(["domReady!"])
     world.css_click(component_button_css)
 
 
