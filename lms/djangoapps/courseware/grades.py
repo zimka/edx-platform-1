Edraak/edx-platform

Optimize grading for unanswered but weighted problems.  Return exception when encountering Error modules when running as task.

lms/djangoapps/courseware/grades.py
@@ -8,13 +8,15 @@ from collections import defaultdict
 from django.conf import settings
 from django.contrib.auth.models import User
 
-from .model_data import ModelDataCache, LmsKeyValueStore
 from xblock.core import Scope
-from .module_render import get_module, get_module_for_descriptor, get_module_for_descriptor_internal
 from xmodule import graders
 from xmodule.capa_module import CapaModule
 from xmodule.graders import Score
-from .models import StudentModule
+from xmodule.error_module import ErrorModule, NonStaffErrorModule
+
+from courseware.model_data import ModelDataCache, LmsKeyValueStore
+from courseware.models import StudentModule
+from courseware.module_render import get_module, get_module_for_descriptor, get_module_for_descriptor_internal
 
 log = logging.getLogger("mitx.courseware")
 
@@ -119,14 +121,14 @@ def answer_distributions(request, course):
     return counts
 
 
-def _grade_section(section_descriptor, student, model_data_cache, create_module):
+def _grade_section(section_descriptor, student, model_data_cache, module_creator):
     """
     Collect scores and grades for a course section
     """
     scores = []
-    for module_descriptor in _yield_dynamic_descriptor_descendents(section_descriptor, create_module):
+    for module_descriptor in _yield_dynamic_descriptor_descendents(section_descriptor, module_creator):
 
-        (correct, total) = _get_score(student, module_descriptor, create_module, model_data_cache)
+        (correct, total) = _get_score(student, module_descriptor, module_creator, model_data_cache)
         if correct is None and total is None:
             continue
 
@@ -171,32 +173,42 @@ def grade(student, request, course, model_data_cache=None, keep_raw_scores=False
     if model_data_cache is None:
         model_data_cache = ModelDataCache(grading_context['all_descriptors'], course.id, student)
 
-    def create_module(descriptor):
+    def module_creator(descriptor):
         '''creates an XModule instance given a descriptor'''
         # TODO: We need the request to pass into here. If we could forego that, our arguments
         # would be simpler
         return get_module_for_descriptor(student, request, descriptor, model_data_cache, course.id)
 
-    return _grade(student, course, grading_context, create_module, model_data_cache, keep_raw_scores)
+    return _grade(student, course, grading_context, module_creator, model_data_cache, keep_raw_scores)
 
 
 def grade_as_task(student, course, track_function, xqueue_callback_url_prefix):
     """
+    TODO:
     """
     course_id = course.id
     grading_context = course.grading_context
     model_data_cache = ModelDataCache(grading_context['all_descriptors'], course_id, student)
     keep_raw_scores = True
 
-    def create_module(descriptor):
+    class ModuleInstantiationException(Exception):
+        pass
+
+    def module_creator(descriptor):
         """creates an XModule instance given a descriptor"""
-        return get_module_for_descriptor_internal(student, descriptor, model_data_cache, course_id,
-                                                  track_function, xqueue_callback_url_prefix)
+        module = get_module_for_descriptor_internal(
+            student, descriptor, model_data_cache, course_id, track_function, xqueue_callback_url_prefix
+        )
+        if isinstance(module, (ErrorModule, NonStaffErrorModule)):
+            raise ModuleInstantiationException("Unable to create module: %s".format(module.error_msg))
 
-    return _grade(student, course, grading_context, create_module, model_data_cache, keep_raw_scores)
+    return _grade(student, course, grading_context, module_creator, model_data_cache, keep_raw_scores)
 
 
-def _grade(student, course, grading_context, create_module_fcn, model_data_cache, keep_raw_scores):
+def _grade(student, course, grading_context, module_creator_fcn, model_data_cache, keep_raw_scores):
+    """
+    TODO:
+    """
     raw_scores = []
     totaled_scores = {}
 
@@ -217,23 +229,14 @@ def _grade(student, course, grading_context, create_module_fcn, model_data_cache
                     should_grade_section = True
                     break
 
-                # Create a fake key to pull out a StudentModule object from the ModelDataCache
-                key = LmsKeyValueStore.Key(
-                    Scope.user_state,
-                    student.id,
-                    moduledescriptor.location,
-                    None
-                )
-                if model_data_cache.find(key):
+                if _get_student_module_from_cache(model_data_cache, student, moduledescriptor) is not None:
                     should_grade_section = True
                     break
 
             if should_grade_section:
-
-                graded_total, scores = _grade_section(section_descriptor, student, model_data_cache, create_module_fcn)
+                graded_total, scores = _grade_section(section_descriptor, student, model_data_cache, module_creator_fcn)
                 if keep_raw_scores:
                     raw_scores += scores
-
             else:
                 graded_total = Score(0.0, 1.0, True, section_name)
 
@@ -365,6 +368,24 @@ def progress_summary(student, request, course, model_data_cache):
     return chapters
 
 
+def _get_student_module_from_cache(model_data_cache, student, problem_descriptor):
+    """
+    Find StudentModule object in model_data_cache.
+
+    Creates a fake KeyValueStore key to pull out the StudentModule,
+    given the `student` object (auth.user) and `problem_descriptor`.
+
+    Returns None if no StudentModule is found.
+    """
+    key = LmsKeyValueStore.Key(
+        Scope.user_state,
+        student.id,
+        problem_descriptor.location,
+        None
+    )
+    return model_data_cache.find(key)
+
+
 def _get_score(user, problem_descriptor, module_creator, model_data_cache):
     """
     Return the score for a user on a problem, as a tuple (correct, total).
@@ -386,8 +407,8 @@ def _get_score(user, problem_descriptor, module_creator, model_data_cache):
         # These are not problems, and do not have a score
         return (None, None)
 
-    # some problems have state that is updated independently of interaction
-    # with the LMS, so they need to always be scored. (E.g. foldit.)
+    # Some problems have state that is updated independently of interaction
+    # with the LMS, so they need to always be scored. (E.g. foldit, open-ended grading.)
     if problem_descriptor.always_recalculate_grades:
         problem = module_creator(problem_descriptor)
         score = problem.get_score()
@@ -396,31 +417,32 @@ def _get_score(user, problem_descriptor, module_creator, model_data_cache):
         else:
             return (None, None)
 
-    # Create a fake KeyValueStore key to pull out the StudentModule
-    key = LmsKeyValueStore.Key(
-        Scope.user_state,
-        user.id,
-        problem_descriptor.location,
-        None
-    )
-
-    student_module = model_data_cache.find(key)
+    student_module = _get_student_module_from_cache(model_data_cache, user, problem_descriptor)
+    weight = problem_descriptor.weight
 
     if student_module is not None and student_module.max_grade is not None:
         correct = student_module.grade if student_module.grade is not None else 0
         total = student_module.max_grade
+        # Now we re-weight the problem, if specified
+        if weight is not None:
+            if total == 0:
+                log.exception("Cannot reweight a problem with zero total points. Problem: " + str(student_module))
+                return (correct, total)
+            correct = correct * weight / total
+            total = weight
+        return (correct, total)
+    elif weight is not None:
+        # If the problem was not in the cache, or hasn't been graded yet,
+        # but has a weight, then we know the max_grade.
+        return (0.0, weight)
+
     else:
         # If the problem was not in the cache, or hasn't been graded yet,
-        # we need to instantiate the problem.
-        # Otherwise, the max score (cached in student_module) won't be available
-
-        # TODO: optimization: if the problem has a weight, then we don't need to
-        # find the total here, we can just return (0, weight).
+        # and has no assigned weight, we need to instantiate the problem.
+        # This will make the max score (cached in student_module) become available.
         problem = module_creator(problem_descriptor)
         if problem is None:
             return (None, None)
-
-        correct = 0.0
         total = problem.max_score()
 
         # Problem may be an error module (if something in the problem builder failed)
@@ -428,13 +450,4 @@ def _get_score(user, problem_descriptor, module_creator, model_data_cache):
         if total is None:
             return (None, None)
 
-    # Now we re-weight the problem, if specified
-    weight = problem_descriptor.weight
-    if weight is not None:
-        if total == 0:
-            log.exception("Cannot reweight a problem with zero total points. Problem: " + str(student_module))
-            return (correct, total)
-        correct = correct * weight / total
-        total = weight
-
-    return (correct, total)
+        return (0.0, total)

zimka/edx-platform-1

lms/djangoapps/courseware/grades.py
@@ -11,11 +11,13 @@ from django.contrib.auth.models import User
 from courseware.model_data import FieldDataCache, DjangoKeyValueStore
 from xblock.fields import Scope
 from .module_render import get_module, get_module_for_descriptor, get_module_for_descriptor_internal
-
 from xmodule import graders
 from xmodule.capa_module import CapaModule
 from xmodule.graders import Score
-from .models import StudentModule
+from xmodule.error_module import ErrorModule, NonStaffErrorModule
+
+from courseware.models import StudentModule
+from courseware.module_render import get_module, get_module_for_descriptor, get_module_for_descriptor_internal
 
 log = logging.getLogger("mitx.courseware")
 
@@ -122,14 +124,14 @@ def answer_distributions(request, course):
     return counts
 
 
-def _grade_section(section_descriptor, student, field_data_cache, create_module):
+def _grade_section(section_descriptor, student, field_data_cache, module_creator):
     """
     Collect scores and grades for a course section
     """
     scores = []
-    for module_descriptor in _yield_dynamic_descriptor_descendents(section_descriptor, create_module):
+    for module_descriptor in _yield_dynamic_descriptor_descendents(section_descriptor, module_creator):
 
-        (correct, total) = _get_score(student, module_descriptor, create_module, field_data_cache)
+        (correct, total) = _get_score(student, module_descriptor, module_creator, field_data_cache)
         if correct is None and total is None:
             continue
 
@@ -139,7 +141,7 @@ def _grade_section(section_descriptor, student, field_data_cache, create_module)
             else:
                 correct = total
 
-        graded = module_descriptor.lms.graded
+        graded = module_descriptor.graded
         if total <= 0:
             # We simply cannot grade a problem that is 12/0, because we might need it as a percentage
             graded = False
@@ -174,32 +176,42 @@ def grade(student, request, course, field_data_cache=None, keep_raw_scores=False
     if field_data_cache is None:
         field_data_cache = FieldDataCache(grading_context['all_descriptors'], course.id, student)
 
-    def create_module(descriptor):
+    def module_creator(descriptor):
         '''creates an XModule instance given a descriptor'''
         # TODO: We need the request to pass into here. If we could forego that, our arguments
         # would be simpler
         return get_module_for_descriptor(student, request, descriptor, field_data_cache, course.id)
 
-    return _grade(student, course, grading_context, create_module, field_data_cache, keep_raw_scores)
+    return _grade(student, course, grading_context, module_creator, field_data_cache, keep_raw_scores)
 
 
 def grade_as_task(student, course, track_function, xqueue_callback_url_prefix):
     """
+    TODO:
     """
     course_id = course.id
     grading_context = course.grading_context
     field_data_cache = FieldDataCache(grading_context['all_descriptors'], course_id, student)
     keep_raw_scores = True
 
-    def create_module(descriptor):
+    class ModuleInstantiationException(Exception):
+        pass
+
+    def module_creator(descriptor):
         """creates an XModule instance given a descriptor"""
-        return get_module_for_descriptor_internal(student, descriptor, field_data_cache, course_id,
-                                                  track_function, xqueue_callback_url_prefix)
+        module = get_module_for_descriptor_internal(
+            student, descriptor, field_data_cache, course_id, track_function, xqueue_callback_url_prefix
+        )
+        if isinstance(module, (ErrorModule, NonStaffErrorModule)):
+            raise ModuleInstantiationException("Unable to create module: %s".format(module.error_msg))
 
-    return _grade(student, course, grading_context, create_module, field_data_cache, keep_raw_scores)
+    return _grade(student, course, grading_context, module_creator, field_data_cache, keep_raw_scores)
 
 
-def _grade(student, course, grading_context, create_module_fcn, field_data_cache, keep_raw_scores):
+def _grade(student, course, grading_context, module_creator_fcn, field_data_cache, keep_raw_scores):
+    """
+    TODO:
+    """
     raw_scores = []
     totaled_scores = {}
 
@@ -220,23 +232,14 @@ def _grade(student, course, grading_context, create_module_fcn, field_data_cache
                     should_grade_section = True
                     break
 
-                # Create a fake key to pull out a StudentModule object from the FieldDataCache
-                key = DjangoKeyValueStore.Key(
-                    Scope.user_state,
-                    student.id,
-                    moduledescriptor.location,
-                    None
-                )
-                if field_data_cache.find(key):
+                if _get_student_module_from_cache(field_data_cache, student, moduledescriptor) is not None:
                     should_grade_section = True
                     break
 
             if should_grade_section:
-                graded_total, scores = _grade_section(section_descriptor, student, field_data_cache, create_module_fcn)
-
+                graded_total, scores = _grade_section(section_descriptor, student, field_data_cache, module_creator_fcn)
                 if keep_raw_scores:
                     raw_scores += scores
-
             else:
                 graded_total = Score(0.0, 1.0, True, section_name)
 
@@ -363,9 +366,24 @@ def progress_summary(student, request, course, field_data_cache):
                          'display_name': chapter_module.display_name_with_default,
                          'url_name': chapter_module.url_name,
                          'sections': sections})
-
     return chapters
 
+def _get_student_module_from_cache(field_data_cache, student, problem_descriptor):
+    """
+    Find StudentModule object in field_data_cache.
+
+    Creates a fake KeyValueStore key to pull out the StudentModule,
+    given the `student` object (auth.user) and `problem_descriptor`.
+
+    Returns None if no StudentModule is found.
+    """
+    key = DjangoKeyValueStore.Key(
+        Scope.user_state,
+        student.id,
+        problem_descriptor.location,
+        None
+    )
+    return field_data_cache.find(key)
 
 def _get_score(user, problem_descriptor, module_creator, field_data_cache):
     """
@@ -392,8 +410,8 @@ def _get_score(user, problem_descriptor, module_creator, field_data_cache):
         # These are not problems, and do not have a score
         return (None, None)
 
-    # some problems have state that is updated independently of interaction
-    # with the LMS, so they need to always be scored. (E.g. foldit.)
+    # Some problems have state that is updated independently of interaction
+    # with the LMS, so they need to always be scored. (E.g. foldit, open-ended grading.)
     if problem_descriptor.always_recalculate_grades:
         problem = module_creator(problem_descriptor)
         if problem is None:
@@ -404,31 +422,32 @@ def _get_score(user, problem_descriptor, module_creator, field_data_cache):
         else:
             return (None, None)
 
-    # Create a fake KeyValueStore key to pull out the StudentModule
-    key = DjangoKeyValueStore.Key(
-        Scope.user_state,
-        user.id,
-        problem_descriptor.location,
-        None
-    )
-
-    student_module = field_data_cache.find(key)
+    student_module = _get_student_module_from_cache(field_data_cache, user, problem_descriptor)
+    weight = problem_descriptor.weight
 
     if student_module is not None and student_module.max_grade is not None:
         correct = student_module.grade if student_module.grade is not None else 0
         total = student_module.max_grade
+        # Now we re-weight the problem, if specified
+        if weight is not None:
+            if total == 0:
+                log.exception("Cannot reweight a problem with zero total points. Problem: " + str(student_module))
+                return (correct, total)
+            correct = correct * weight / total
+            total = weight
+        return (correct, total)
+    elif weight is not None:
+        # If the problem was not in the cache, or hasn't been graded yet,
+        # but has a weight, then we know the max_grade.
+        return (0.0, weight)
+
     else:
         # If the problem was not in the cache, or hasn't been graded yet,
-        # we need to instantiate the problem.
-        # Otherwise, the max score (cached in student_module) won't be available
-
-        # TODO: optimization: if the problem has a weight, then we don't need to
-        # find the total here, we can just return (0, weight).
+        # and has no assigned weight, we need to instantiate the problem.
+        # This will make the max score (cached in student_module) become available.
         problem = module_creator(problem_descriptor)
         if problem is None:
             return (None, None)
-
-        correct = 0.0
         total = problem.max_score()
 
         # Problem may be an error module (if something in the problem builder failed)
@@ -436,13 +455,4 @@ def _get_score(user, problem_descriptor, module_creator, field_data_cache):
         if total is None:
             return (None, None)
 
-    # Now we re-weight the problem, if specified
-    weight = problem_descriptor.weight
-    if weight is not None:
-        if total == 0:
-            log.exception("Cannot reweight a problem with zero total points. Problem: " + str(student_module))
-            return (correct, total)
-        correct = correct * weight / total
-        total = weight
-
-    return (correct, total)
+        return (0.0, total)