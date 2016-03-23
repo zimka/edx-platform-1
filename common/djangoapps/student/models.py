efad2c509ef20e504cbbdf1953ff1d4d243471b7
Pavel Martynov <martynovp@gmail.com>
2016-03-23 11:05:25 +0000

    EDX-269 Additional user info for logging login and logout events

--- common/djangoapps/student/models.py
+++ common/djangoapps/student/models.py
@@ -1720,7 +1720,7 @@ def log_successful_login(sender, request, user, **kwargs):  # pylint: disable=un
     if settings.FEATURES['SQUELCH_PII_IN_LOGS']:
         AUDIT_LOG.info(u"Login success - user.id: {0}".format(user.id))
     else:
-        AUDIT_LOG.info(u"Login success - {0} ({1})".format(user.username, user.email))
+        AUDIT_LOG.info(u"Login success - user.id: {0}; username: {1}; email: {2}; ip: {3}".format(user.id, user.username, user.email, request.META.get('REMOTE_ADDR', 'unknown')))
 
 
 @receiver(user_logged_out)
@@ -1729,7 +1729,10 @@ def log_successful_logout(sender, request, user, **kwargs):  # pylint: disable=u
     if settings.FEATURES['SQUELCH_PII_IN_LOGS']:
         AUDIT_LOG.info(u"Logout - user.id: {0}".format(request.user.id))
     else:
-        AUDIT_LOG.info(u"Logout - {0}".format(request.user))
+        if request.user.id:
+            AUDIT_LOG.info(u"Logout - user.id: {0}; username: {1}; email: {2}; ip: {3}".format(request.user.id, request.user.username, request.user.email, request.META.get('REMOTE_ADDR', 'unknown')))
+        else:
+            AUDIT_LOG.info(u"Logout - user.id: None; ip: {0}; uri: {1}".format(request.META.get('REMOTE_ADDR', 'unknown'), request.META.get('RAW_URI', 'unknown').split('?')[0]))
 
 
 @receiver(user_logged_in)
