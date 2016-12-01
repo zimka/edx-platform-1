1217900791cf478fc1933d82541cf51bbbdcfcf2
Boris Zimka <mystsleep@gmail.com>
2016-12-01 17:43:35 +0000

    Stable v0.1. All command combos checked manually

--- common/djangoapps/student/management/commands/cohort_change_due.py
+++ common/djangoapps/student/management/commands/cohort_change_due.py
@@ -1,19 +1,29 @@
 # -*- coding: utf-8 -*-
+import dateutil
+from datetime import timedelta
 from django.core.management.base import BaseCommand, CommandError
 from django.utils.timezone import utc
 from lms.djangoapps.instructor.views.tools import require_student_from_identifier, set_due_date_extension
-from django.contrib.auth.models import User
-import sys
-import dateutil
+from openedx.core.djangoapps.course_groups.cohorts import get_cohort_by_id, get_cohort_by_name
 from opaque_keys.edx.keys import CourseKey, UsageKey
-from openedx.core.djangoapps.course_groups.cohorts import get_course_cohorts, get_course_cohort_settings
-from xmodule.modulestore.django import modulestore, contentstore
 from student.models import CourseEnrollment
+from xmodule.modulestore.django import modulestore
 
 
 class Command(BaseCommand):
-    help = """
+    help = u"""
     Changes due date for given problem for given cohort or user
+    """ + u"""
+    Команде нужно указать 3 параметра: для кого менять, где менять и на когда менять
+    Кто:
+        --user={username / emmail},
+        --cohort={name/id[ не тестировал]}
+    Где:
+        --problem_location={Location проблемы из STAFF DEBUG INFO в LMS}
+        --course_key={id курса, например 'course-v1:test_o+test_n+test_r'}
+    Когда:
+        --date_set={дата формата 2017/01/31}
+        --days_add={int добавить дней. Команда с этой опцией не кумулятивна.}
     """
 
     def add_arguments(self, parser):
@@ -28,7 +38,6 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         store = modulestore()
-        self.stdout.write("Hello command world")
 
         who_keys = ('cohort', 'user')
         where_keys = ('problem_location', 'course_key')
@@ -39,9 +48,10 @@ class Command(BaseCommand):
         when_key, when_val = self._exist_n_unique(options, when_keys)
 
         choose_keys = [who_key, where_key, when_key]
-        changer_wrap = self.choose_date_changer(choose_keys)
+        changer_wrap = choose_date_changer(choose_keys)
         changer = changer_wrap(who_val, where_val, when_val, store, stdout=self.stdout)
         changer.change_due()
+        self.stdout.write("Successfully changed date.")
 
     @staticmethod
     def _exist_n_unique(container, keys):
@@ -59,14 +69,45 @@ class Command(BaseCommand):
         single_key = exist_pairs[0][0]
         return single_key, container[single_key]
 
-    @staticmethod
-    def choose_date_changer(keys):
-        key_set = set(keys)
-        if len(key_set) != 3:
-            raise CommandError("Internal Error. To much keys:{}".format(keys))
-        user_set_date_keys = ['user', 'date_set', 'problem_location']
-        if all(x in key_set for x in user_set_date_keys):
-            return UserSetProblemDate.wrap()
+
+def choose_date_changer(keys):
+    key_set = set(keys)
+    if len(key_set) != 3:
+        raise CommandError("Internal Error. To much keys:{}".format(keys))
+
+    user_problem_set_date_keys = ['user', 'date_set', 'problem_location']
+    if all(x in key_set for x in user_problem_set_date_keys):
+        return UserProblemSetDate.wrap()
+
+    user_problem_add_days_keys = ['user', 'days_add', 'problem_location']
+    if all(x in key_set for x in user_problem_add_days_keys):
+        return UserProblemAddDays.wrap()
+
+    user_course_set_date_keys = ['user', 'date_set', 'course_key']
+    if all(x in key_set for x in user_course_set_date_keys):
+        return UserCourseSetDate.wrap()
+
+    user_course_add_days_keys = ['user', 'days_add', 'course_key']
+    if all(x in key_set for x in user_course_add_days_keys):
+        return UserCourseAddDays.wrap()
+
+    cohort_problem_set_date_keys = ['cohort', 'date_set', 'problem_location']
+    if all(x in key_set for x in cohort_problem_set_date_keys):
+        return CohortProblemSetDate.wrap()
+
+    cohort_problem_add_days_keys = ['cohort', 'days_add', 'problem_location']
+    if all(x in key_set for x in cohort_problem_add_days_keys):
+        return CohortProblemAddDays.wrap()
+
+    cohort_course_set_date_keys = ['cohort', 'date_set', 'course_key']
+    if all(x in key_set for x in cohort_course_set_date_keys):
+        return CohortCourseSetDate.wrap()
+
+    cohort_course_add_days_keys = ['cohort', 'days_add', 'course_key']
+    if all(x in key_set for x in cohort_course_add_days_keys):
+        return CohortCourseAddDays.wrap()
+
+    raise NotImplementedError("Keys combination not recognized")
 
 
 class DateChanger(object):
@@ -113,26 +154,203 @@ class DateChanger(object):
     def wrap(cls):
         def inner_wrap(*args, **kwargs):
             return cls(*args, **kwargs)
+
         return inner_wrap
 
+    @property
+    def users_group(self):
+        raise NotImplementedError
 
-class UserSetProblemDate(DateChanger):
+    @property
+    def xblock_group(self):
+        raise NotImplementedError
+
+    @property
+    def dates_group(self):
+        raise NotImplementedError
+
+    @property
+    def course(self):
+        raise NotImplementedError
+
     def change_due(self):
-        store = self.store
+        for user in self.users_group:
+            for num, xblock in enumerate(self.xblock_group):
+                date = self.dates_group[num]
 
+                is_enrolled = CourseEnrollment.is_enrolled(user, self.course.id)
+                if not is_enrolled:
+                    raise CommandError("User {} is not enrolled on course with key {}".format(
+                        user.username,
+                        self.course.id))
+
+                set_due_date_extension(self.course, xblock, user, date)
+
+
+class ProblemMixin(object):
+    @property
+    def xblock_group(self):
+        try:
+            return self._xblock_group
+        except AttributeError:
+            pass
         usage_key = UsageKey.from_string(self.where)
-        xblock = store.get_item(usage_key)
-        if not xblock:
-            raise CommandError("Didn't find xblock for location {}".format(self.where))
+        xblock = self.store.get_item(usage_key)
+        if not getattr(self, "_course", False):
+            self._course = self._get_xblock_course(xblock)
+        self._xblock_group = [xblock]
 
-        user = require_student_from_identifier(self.who)
+        return self._xblock_group
 
-        date = self._parse_date(self.when)
+    @property
+    def course(self):
+        try:
+            return self._course
+        except AttributeError:
+            xblock = self.xblock_group[0]
+            self._course = self._get_xblock_course(xblock)
+            return self._course
+        raise NotImplementedError
 
-        course = self._get_xblock_course(xblock)
-        course_key = course.id
 
-        is_enrolled = CourseEnrollment.is_enrolled(user, course_key)
-        if not is_enrolled:
-            raise CommandError("User {} is not enrolled on course with key {}".format(user.username, course_key))
-        set_due_date_extension(course, xblock, user, date)
+class CourseMixin(object):
+    @property
+    def xblock_group(self):
+        try:
+            return self._xblock_group
+        except AttributeError:
+            pass
+
+        course_key = CourseKey.from_string(self.where)
+        course = self.store.get_course(course_key)
+        if not getattr(self, "_course", False):
+            self._course = course
+
+        items = self.store.get_items(course_key)
+        items = [x for x in items if (getattr(x, "due", False) and x.format)]
+        self._xblock_group = items
+        return self._xblock_group
+
+    @property
+    def course(self):
+        try:
+            return self._course
+        except AttributeError:
+            pass
+        course_key = CourseKey.from_string(self.where)
+        self._course = self.store.get_course(course_key)
+        return self._course
+
+
+class UserMixin(object):
+    @property
+    def users_group(self):
+        try:
+            return self._users_group
+        except AttributeError:
+            pass
+        self._users_group = [require_student_from_identifier(self.who)]
+        return self._users_group
+
+
+class CohortMixin(object):
+    def _get_cohort(self, cohort_id_or_name):
+        course_key = self.course.id
+        try:
+            cohort = get_cohort_by_name(course_key,cohort_id_or_name)
+            return cohort
+        except Exception as e:
+            pass
+        try:
+            cohort = get_cohort_by_id(course_key, cohort_id_or_name)
+            return cohort
+        except Exception as e:
+            pass
+        raise CommandError("Didn't find cohort")
+
+    @property
+    def users_group(self):
+        try:
+            return self._users_group
+        except AttributeError:
+            pass
+        cohort = self._get_cohort(self.who)
+        self._users_group = list(cohort.users.all())
+        return self._users_group
+
+
+class SetDateMixin(object):
+    @property
+    def dates_group(self):
+        try:
+            return self._dates_group
+        except AttributeError:
+            pass
+        self._dates_group = [self._parse_date(self.when)]*len(self.xblock_group)
+        return self._dates_group
+
+
+class AddDaysMixin(object):
+    """
+    IMPORTANT
+    При текущей реализации прибавление дней не кумулятивно -
+    в качестве базы берется значение due самого xblock, а не
+    StudentFieldOverride, поэтому если выполнение команды дважды
+    приведет к такому же результату, как и однократное.
+    """
+
+    @property
+    def dates_group(self):
+        try:
+            return self._dates_group
+        except AttributeError:
+            pass
+        xblocks = self.xblock_group
+        dates = [x.due for x in xblocks]
+        dates = [d + timedelta(days=int(self.when)) for d in dates]
+        self._dates_group = dates
+        return self._dates_group
+
+
+class UserProblemSetDate(UserMixin, ProblemMixin, SetDateMixin, DateChanger):
+    pass
+
+
+class UserProblemAddDays(UserMixin, ProblemMixin, AddDaysMixin, DateChanger):
+    pass
+
+
+class UserCourseSetDate(UserMixin, CourseMixin, SetDateMixin, DateChanger):
+    pass
+
+
+class UserCourseAddDays(UserMixin, CourseMixin, AddDaysMixin, DateChanger):
+    pass
+
+
+class CohortProblemSetDate(CohortMixin, ProblemMixin, SetDateMixin, DateChanger):
+    pass
+
+
+class CohortProblemAddDays(CohortMixin, ProblemMixin, AddDaysMixin, DateChanger):
+    pass
+
+
+class CohortCourseSetDate(CohortMixin, CourseMixin, SetDateMixin, DateChanger):
+    pass
+
+
+class CohortCourseAddDays(CohortMixin, CourseMixin, AddDaysMixin, DateChanger):
+    pass
+
+
+
+"""
+python manage.py lms cohort_change_due --course_key=course-v1:test_o+test_n+test_r --days_add=30 --cohort=my_cohort
+python manage.py lms cohort_change_due --course_key=course-v1:test_o+test_n+test_r --date_set=2016/12/12 --cohort=my_cohort
+python manage.py lms cohort_change_due
+    --problem_location=block-v1:test_o+test_n+test_r+type@problem+block@b725c8e540884a7890e0711b3f59a0aa
+    --date_set="2016/12/10"
+    --user=test1@test1.com
+
+"""
