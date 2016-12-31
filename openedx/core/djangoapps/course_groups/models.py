c022a053d949b5565c45eec4988e74ec9860cdd9
martynovp <martynovp@gmail.com>
2016-12-31 07:26:28 +0300

    EDX-366 Рефакторинг апдейта когорты verified

--- openedx/core/djangoapps/course_groups/models.py
+++ openedx/core/djangoapps/course_groups/models.py
@@ -102,7 +102,8 @@ class CohortMembership(models.Model):
         # This block will transactionally commit updates to CohortMembership and underlying course_user_groups.
         # Note the use of outer_atomic, which guarantees that operations are committed to the database on block exit.
         # If called from a view method, that method must be marked with @transaction.non_atomic_requests.
-        with outer_atomic(read_committed=True):
+        # with outer_atomic(read_committed=True):
+        if True:  # in openedu we call this method already from atomic block
 
             saved_membership, created = CohortMembership.objects.select_for_update().get_or_create(
                 user__id=self.user.id,
