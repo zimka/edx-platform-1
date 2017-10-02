82ef1ba81a11bd99238bc3d1a37cf9abf4203d9a
Boris Zimka <mystsleep@gmail.com>
2017-10-02 11:12:08 +0000

    Course Shift changes

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -277,3 +277,9 @@ if os.path.isfile(join(dirname(abspath(__file__)), 'private.py')):
 MODULESTORE = convert_module_store_setting_if_needed(MODULESTORE)
 
 SECRET_KEY = '85920908f28904ed733fe576320db18cabd7b6cd'
+INSTALLED_APPS += ('course_shifts',)
+FEATURES["ENABLE_COURSE_SHIFTS"] = True
+FIELD_OVERRIDE_PROVIDERS += (
+    'course_shifts.provider.CourseShiftOverrideProvider',
+)
+EDX_API_KEY = 'EDX_API_KEY'
--- lms/static/js/instructor_dashboard/instructor_dashboard.js
+++ lms/static/js/instructor_dashboard/instructor_dashboard.js
@@ -196,6 +196,9 @@ such that the value can be defined later than this assignment (file load order).
             }, {
                 constructor: window.InstructorDashboard.sections.OpenResponseAssessment,
                 $element: idashContent.find('.' + CSS_IDASH_SECTION + '#open_response_assessment')
+            }, {
+                constructor: window.InstructorDashboard.sections.CourseShifts,
+                $element: idashContent.find('.' + CSS_IDASH_SECTION + '#course_shifts')
             }
         ];
         if (edx.instructor_dashboard.proctoring !== void 0) {
